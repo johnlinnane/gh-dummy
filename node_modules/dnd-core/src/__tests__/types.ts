b9a322a9c6bf7085ab892735dd7ed3b1fc1fc3f3
Panister Fatheroy <panisterfatheroy@gmail.com>
2020-07-27 13:04:33 +0100

    delete most

--- node_modules/dnd-core/src/__tests__/types.ts
+++
@@ -1,3 +0,0 @@
-export const FOO = 'FOO'
-export const BAR = 'BAR'
-export const BAZ = 'BAZ'
--- node_modules/dnd-core/src/reducers/stateId.ts
+++
@@ -1,5 +0,0 @@
-export type State = number
-
-export default function stateId(state: State = 0) {
-	return state + 1
-}
